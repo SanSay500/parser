5d76aa5dcbb27e0ec819540182fc09e01d2aa4d0
sansay500 <borodachev@gmail.com>
2022-07-30 14:02:08 +0300

    avtoz parsing +

--- index.php
+++ index.php
@@ -2,11 +2,15 @@
 require_once __DIR__.'/vendor/autoload.php';
 use Symfony\Component\DomCrawler\Crawler;
 
-$crawler = new Crawler('http://avtoz1.nnov.msudrf.ru/modules.php?name=terr');
+$crawler = new Crawler();
+$crawler->add(file_get_contents('http://avtoz1.nnov.msudrf.ru/modules.php?name=terr'));
 
-$nodeValues = $crawler->filterXPath('//div[@class="left"]')->text();
-// ->each(function (Crawler $node, $i) {
-//     return $node->text();
-// });
 
+$nodeValues = $crawler->filterXPath('//div[@class="right"]')
+ ->each(function (Crawler $node, $i) {
+     return $node->text();
+ });
+
+echo '<pre>';
 var_dump($nodeValues);
+echo '</pre>';
